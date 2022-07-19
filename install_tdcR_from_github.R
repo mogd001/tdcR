ece81c510da138bf119cf621d290bf2240d67ea9
matt.ogden <matt.ogden@tasman.govt.nz>
2022-07-19 13:39:02 +1200

    Commit install_tdcR_from_github.R

---
+++ install_tdcR_from_github.R
@@ -0,0 +1,4 @@
+library(devtools)
+# requires personal access token
+# https://github.com/settings/tokens/new
+install_github("mogd001/tdcR", auth_token = "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")
