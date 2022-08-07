f8d64563536c05d2b835bf492357983272a5b243
matt.ogden <mogd001@gmail.com>
2022-08-08 10:16:52 +1200

    Updated options for install

--- install_tdcR_from_github.R
+++ install_tdcR_from_github.R
@@ -1,4 +1,5 @@
 library(devtools)
+options(download.file.method = "wininet")
 # requires personal access token
 # https://github.com/settings/tokens/new
 install_github("mogd001/tdcR", auth_token = "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")
