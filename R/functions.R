44438d419cea670f686e583d69c8776fe0932916
matt.ogden <mogd001@gmail.com>
2022-08-20 10:40:31 +1200

    Added first envmon functions

--- R/functions.R
+++ R/functions.R
@@ -8,9 +8,11 @@
 #' @import zeallot
 #' @import rapport
 #' @import stringr
+#' @import RODBC
 
 # TODO - improve function definitions.
 
+########## HILLTOP SERVER ##########
 get_sites <- function(latlong = TRUE) {
   # Function to get Sites from Hilltop Server.
   url <- "http://envdata.tasman.govt.nz/data.hts?Service=Hilltop&Request=SiteList"
@@ -300,4 +302,84 @@ load_gaugings <- function(site, start_date, end_date) {
     ) # filter ratings to analysis period
 
   return(gaugings)
+}
+
+
+########## ENVMON ##########
+read_site_information_from_envmon <- function(site) {
+  # Function to read site information from envmon database.
+  envmon_string <-
+    "driver={SQL Server};server=TSRVSQL14;database=ENVMON;trusted_connection=true"
+  envmon <- odbcDriverConnect(envmon_string)
+  site_name_wrapped <- paste0("\'", site, "\'")
+  site_information <- sqlQuery(
+    envmon,
+    paste0(
+      "SELECT SiteID, Name, Easting, Northing, AuxName1, AuxName2, CatchmentArea, HIRDS
+     FROM Site",
+      " WHERE Name LIKE ",
+      site_name_wrapped
+    ),
+    stringsAsFactors = FALSE
+  ) %>%
+    rename(
+      site_id = SiteID,
+      site = Name,
+      easting = Easting,
+      northing = Northing,
+      first_synonym = AuxName1,
+      second_synonym = AuxName2,
+      catchment_area = CatchmentArea,
+      hirds_data = HIRDS
+    )
+  # Close db connection
+  odbcClose(envmon)
+
+  return(site_information)
+}
+
+
+tabulate_hirds_data_string <- function(data_string) {
+  # Function to map HIRDS string saved in ENVMON database to tibble.
+  col_names <-
+    c(
+      "ARI",
+      "10 min",
+      "20 min",
+      "30 min",
+      "1 hour",
+      "2 hour",
+      "6 hour",
+      "12 hour",
+      "1 day",
+      "2 day",
+      "3 day",
+      "4 day",
+      "5 day"
+    )
+
+  # Split string according to \r\n rows, then convert rows to double and create dataframe.
+  rows <- strsplit(data_string, "\r\n")[[1]]
+
+  n_col <- 0
+  for (row in rows) {
+    x <- as.double(unlist(strsplit(row, ",")))
+    if (n_col == 0) {
+      n_col <- length(x)
+      v <- x
+    } else {
+      v <- append(v, x)
+    }
+  }
+  m <- t(matrix(v, ncol = n_col))
+
+  # add empty columns to m
+  for (i in 1:(length(col_names) - n_col)) {
+    m <- cbind(m, rep(NA, length(m[, 1])))
+  }
+
+  hirds_df <- as_tibble(m)
+  names(hirds_df) <- col_names
+
+  return(hirds_df)
 }
